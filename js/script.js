6e02488133339f88fca4874d26be239d2fb8a9f9
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-24 13:07:12 +0530

    Add projects section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const button = document.getElementById("projects-button");
+
+button.onclick = () => (window.location.href = "projects.html");
+
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
