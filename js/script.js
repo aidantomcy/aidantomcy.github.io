22406d1c3ba6f40f58bd2cf5675b1c75b0624608
Aidan Tomcy <aidantomcy@gmail.com>
2022-02-24 13:27:28 +0530

    Refactor button code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-const button = document.getElementById("projects-button");
-
-button.onclick = () => (window.location.href = "projects.html");
-
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
