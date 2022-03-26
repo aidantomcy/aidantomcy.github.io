09031b7cb243d52cf1d3844036d2adbf39a081e8
Aidan Tomcy <aidantomcy@gmail.com>
2022-03-26 13:19:51 +0530

    Add code to save theme to locatStorage

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ const setLight = () => {
   darkLightBtn.innerHTML = '<div class="pb-2">Let there be darkness</div>';
   document.body.classList.remove("dark");
   document.body.classList.add("light");
+
+  localStorage.setItem("theme", "light");
 };
 
 const setDark = () => {
@@ -21,9 +23,17 @@ const setDark = () => {
   darkLightBtn.innerHTML = '<div class="pb-2">Let there be light</div>';
   document.body.classList.remove("light");
   document.body.classList.add("dark");
+  localStorage.setItem("theme", "dark");
 };
 
-document.body.classList.add("dark");
+if (localStorage.getItem("theme") == null) {
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("theme") == "dark") {
+  setDark();
+} else {
+  setLight();
+}
+
 darkLightBtn.addEventListener("click", () => {
   if (document.body.classList.contains("dark")) {
     setLight();
