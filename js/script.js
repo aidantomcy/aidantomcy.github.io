8ff66f5395eda25813c8367f9e85342c4c38043c
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-23 18:28:14 +0530

    Refactor dark and light mode switching code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 const darkLightBtn = document.getElementById("dark-light-toggle");
-const root = document.querySelector(":root");
+// const root = document.querySelector(":root");
 
 const setLight = () => {
-  root.style.setProperty("--background-color", "#fff");
-  root.style.setProperty("--text-color", "#333");
-  root.style.setProperty("--hover-text-color", "#000");
-  root.style.setProperty("--text-gray-300", "#ccc");
-  root.style.setProperty("--text-gray-500", "#a0aec0");
   darkLightBtn.innerHTML = '<div class="pb-2">Let there be darkness</div>';
   document.body.classList.remove("dark");
   document.body.classList.add("light");
@@ -15,11 +10,6 @@ const setLight = () => {
 };
 
 const setDark = () => {
-  root.style.setProperty("--background-color", "#1f1f1f");
-  root.style.setProperty("--text-color", "#ccc");
-  root.style.setProperty("--hover-text-color", "#fff");
-  root.style.setProperty("--text-gray-300", "#cbd5e1");
-  root.style.setProperty("--text-gray-500", "#6b7280");
   darkLightBtn.innerHTML = '<div class="pb-2">Let there be light</div>';
   document.body.classList.remove("light");
   document.body.classList.add("dark");
