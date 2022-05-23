bd04750332b5e0d3071c9d2d8ae2ef4d84b3ad83
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-23 18:35:10 +0530

    Configure project to use TypeScript

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
-const darkLightBtn = document.getElementById("dark-light-toggle");
-
+"use strict";
+const darkLightBtn = (document.getElementById("dark-light-toggle"));
 const setLight = () => {
-  darkLightBtn.innerHTML = '<div class="pb-2">Let there be darkness</div>';
-  document.body.classList.remove("dark");
-  document.body.classList.add("light");
-
-  localStorage.setItem("theme", "light");
+    darkLightBtn.innerHTML = '<div class="pb-2">Let there be darkness</div>';
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
 };
-
 const setDark = () => {
-  darkLightBtn.innerHTML = '<div class="pb-2">Let there be light</div>';
-  document.body.classList.remove("light");
-  document.body.classList.add("dark");
-  localStorage.setItem("theme", "dark");
+    darkLightBtn.innerHTML = '<div class="pb-2">Let there be light</div>';
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
 };
-
 if (localStorage.getItem("theme") === null) {
-  document.body.classList.add("dark");
-} else if (localStorage.getItem("theme") === "dark") {
-  setDark();
-} else {
-  setLight();
+    document.body.classList.add("dark");
 }
-
-darkLightBtn.addEventListener("click", () => {
-  if (document.body.classList.contains("dark")) {
-    setLight();
-  } else {
+else if (localStorage.getItem("theme") === "dark") {
     setDark();
-  }
+}
+else {
+    setLight();
+}
+darkLightBtn.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+        setLight();
+    }
+    else {
+        setDark();
+    }
 });
-
 document.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 });
---
+++ ts/script.ts
@@ -0,0 +1,38 @@
+const darkLightBtn = <HTMLButtonElement>(
+  document.getElementById("dark-light-toggle")
+);
+
+const setLight = (): void => {
+  darkLightBtn.innerHTML = '<div class="pb-2">Let there be darkness</div>';
+  document.body.classList.remove("dark");
+  document.body.classList.add("light");
+
+  localStorage.setItem("theme", "light");
+};
+
+const setDark = (): void => {
+  darkLightBtn.innerHTML = '<div class="pb-2">Let there be light</div>';
+  document.body.classList.remove("light");
+  document.body.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+};
+
+if (localStorage.getItem("theme") === null) {
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("theme") === "dark") {
+  setDark();
+} else {
+  setLight();
+}
+
+darkLightBtn.addEventListener("click", () => {
+  if (document.body.classList.contains("dark")) {
+    setLight();
+  } else {
+    setDark();
+  }
+});
+
+document.addEventListener("contextmenu", (e: Event) => {
+  e.preventDefault();
+});
