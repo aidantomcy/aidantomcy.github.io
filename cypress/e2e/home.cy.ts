3f796a96a24ba3eef3b908c1f38687cde97cd061
Aidan Tomcy <aidantomcy@gmail.com>
2022-07-17 07:20:15 +0530

    Create seperate port variable and include it in baseUrl

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe("My Portfolio Website", () => {
-  const baseUrl = "http://localhost:3000";
+  const port = 3000;
+  const baseUrl = `http://localhost:${port}`;
   beforeEach(() => {
     cy.visit(baseUrl);
   });
