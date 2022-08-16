8e9a86fdce26c4ab70ab040bd472556e63594f97
Aidan Tomcy <aidantomcy@gmail.com>
2022-08-16 13:31:25 +0530

    Add array with possible text options for button in theme.ts

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,12 +1,14 @@
+const btnText = ["Let there be darkness", "Let there be light"];
+
 const setLight = (btn: HTMLButtonElement): void => {
-  btn.textContent = "Let there be darkness";
+  btn.textContent = btnText[0];
   document.body.classList.remove("dark");
   document.body.classList.add("light");
   localStorage.setItem("theme", "light");
 };
 
 const setDark = (btn: HTMLButtonElement): void => {
-  btn.textContent = "Let there be light";
+  btn.textContent = btnText[1];
   document.body.classList.remove("light");
   document.body.classList.add("dark");
   localStorage.setItem("theme", "dark");
