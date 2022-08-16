eb7c93ee14889588fab56f0fff7bbd770080940e
Aidan Tomcy <aidantomcy@gmail.com>
2022-08-16 13:34:22 +0530

    Add type Themes to store values for available themes

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,10 +1,13 @@
+type Themes = "dark" | "light";
 const btnText = ["Let there be darkness", "Let there be light"];
+let currentTheme: Themes = "dark";
 
 const setLight = (btn: HTMLButtonElement): void => {
+  currentTheme = "light";
   btn.textContent = btnText[0];
   document.body.classList.remove("dark");
-  document.body.classList.add("light");
-  localStorage.setItem("theme", "light");
+  document.body.classList.add(currentTheme);
+  localStorage.setItem("theme", currentTheme);
 };
 
 const setDark = (btn: HTMLButtonElement): void => {
