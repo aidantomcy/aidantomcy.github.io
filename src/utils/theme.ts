b86f88968eacf12686de094cc184e291e6cf1347
Aidan Tomcy <aidantomcy@gmail.com>
2022-08-28 18:03:14 +0530

    Add BtnText type and use it in the setTheme function

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,8 +1,10 @@
 type Themes = "dark" | "light";
-const btnText = ["Let there be darkness", "Let there be light"];
+type BtnText = "Let there be darkness" | "Let there be light";
+let btnText: BtnText = "Let there be darkness";
 
 const setTheme = (theme: Themes, btn: HTMLButtonElement): void => {
-  btn.textContent = theme === "dark" ? btnText[1] : btnText[0];
+  btnText = theme === "dark" ? "Let there be light" : "Let there be darkness";
+  btn.textContent = btnText;
   document.body.classList.remove(theme === "dark" ? "light" : "dark");
   document.body.classList.add(theme);
   localStorage.setItem("theme", theme);
