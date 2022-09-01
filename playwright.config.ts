cf2ee07d7f2b16b0375131153e5502e6ec9a54db
Aidan Tomcy <aidantomcy@gmail.com>
2022-09-01 14:58:13 +0530

    Update test timeout and enable trace in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 30 * 10000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -40,7 +40,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
