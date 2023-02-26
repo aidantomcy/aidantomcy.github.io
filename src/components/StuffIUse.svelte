ee0909448d54a65a16b6f6b153ae1f234e13798f
Aidan Tomcy <aidantomcy@gmail.com>
2023-02-26 19:47:36 +0530

    added stuff i use section

---
+++ src/components/StuffIUse.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { stuffIUse } from "./utils/stuffIUse";
+</script>
+
+<div
+  class="font-bold text-3xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16 ml-[27rem]"
+  id="contact"
+>
+  Stuff I Use
+</div>
+<div class="flex ml-[27rem]">
+  {#each stuffIUse as tool}
+    <img
+      src={`/${tool}.svg`}
+      alt=""
+      class="w-10 grayscale hover:grayscale-0 pr-1"
+    />
+  {/each}
+</div>
---
+++ src/components/utils/stuffIUse.ts
@@ -0,0 +1,14 @@
+export const stuffIUse = [
+  "typescript",
+  "rust",
+  "python",
+  "php",
+  "javascript",
+  "reactjs",
+  "nextjs",
+  "sass",
+  "tailwindcss",
+  "expressjs",
+  "playwright",
+  "mongodb",
+];
