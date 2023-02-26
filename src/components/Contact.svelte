decd0cb62f41dc767fbbf21d90bfd18e6b8f3604
Aidan Tomcy <aidantomcy@gmail.com>
2023-02-26 19:09:46 +0530

    added contact section

---
+++ src/components/Contact.svelte
@@ -0,0 +1,13 @@
+<div
+  class="font-bold text-3xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16 ml-[27rem]"
+  id="contact"
+>
+  Contact
+</div>
+<p class="ml-[27rem] text-[1.2rem] mb-6">
+  <span class="font-semibold tracking-wide">Email</span>:
+  <a href="mailto:aidantomcy@gmail.com" class="text-gray-200 hover:text-white"
+    >aidantomcy@gmail.com</a
+  > <br />
+  <span class="font-semibold tracking-wide">Phone</span>: (I don't have a phone)
+</p>
