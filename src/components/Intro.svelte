078b6e7496d807929e22591365697744438236e2
Aidan <80614206+aidantomcy@users.noreply.github.com>
2023-11-19 07:10:16 +0000

    rename About.svelte -> Intro.svelte

---
+++ src/components/Intro.svelte
@@ -0,0 +1,11 @@
+<section class="flex flex-col mx-8 lg:flex-row lg:justify-between mt-8">
+  <div class="mb-10">
+    <h1 class="text-4xl font-semibold tracking-wide">aidan tomcy</h1>
+    <div class="text-gray-700 dark:text-gray-400 text-md pt-2">
+      TypeScript and Rust developer.
+    </div>
+  </div>
+  <div>
+    <img src="/aidan.jpg" alt="" class="w-40 rounded-2xl lg:w-48" />
+  </div>
+</section>
