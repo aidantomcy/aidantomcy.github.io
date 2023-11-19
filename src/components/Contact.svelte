e4355047e7d07216020bd5a05544746c6480abe2
Aidan <80614206+aidantomcy@users.noreply.github.com>
2023-11-19 07:10:50 +0000

    fix dark mode issues

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,13 +1,18 @@
-<div
-  class="font-bold text-3xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16 ml-[27rem]"
-  id="contact"
->
-  Contact
-</div>
-<p class="ml-[27rem] text-[1.2rem] mb-6">
-  <span class="font-semibold tracking-wide">Email</span>:
-  <a href="mailto:aidantomcy@gmail.com" class="text-gray-200 hover:text-white"
-    >aidantomcy@gmail.com</a
-  > <br />
-  <span class="font-semibold tracking-wide">Phone</span>: (I don't have a phone)
-</p>
+<section class="mx-8 mb-16">
+  <div
+    class="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16"
+    id="contact"
+  >
+    Contact
+  </div>
+  <p class="text-[1.2rem] mb-6">
+    <span class="font-semibold tracking-wide">Email</span>:
+    <a
+      href="mailto:aidantomcy@gmail.com"
+      class="dark:text-gray-200 hover:underline hover:decoration-sky-500 hover:decoration-wavy"
+      >aidantomcy@gmail.com</a
+    > <br />
+    <span class="font-semibold tracking-wide">Phone</span>: (I don't have a
+    phone)
+  </p>
+</section>
