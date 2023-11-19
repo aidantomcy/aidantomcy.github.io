50f4a1db4ffa51cb2d25767b71797babaf2bbe0d
Aidan Tomcy <aidantomcy@gmail.com>
2023-11-19 20:46:35 +0530

    install partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  site: 'https://aidantomcy.github.io',
+  integrations: [tailwind(), svelte(), partytown()],
+  site: 'https://aidantomcy.github.io'
 });
