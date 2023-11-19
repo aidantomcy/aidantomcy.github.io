82caa7c420cb5fdc0b83471abf9cd56e04bcf49f
Aidan <80614206+aidantomcy@users.noreply.github.com>
2023-11-19 07:12:04 +0000

    optimize for smaller screens

--- src/components/StuffIUse.svelte
+++ src/components/StuffIUse.svelte
@@ -2,19 +2,16 @@
   import { stuffIUse } from "./utils/stuffIUse";
 </script>
 
-<div
-  class="font-bold text-3xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16 ml-[27rem]"
-  id="contact"
->
-  Stuff I Use
-</div>
-<div class="flex ml-[27rem]">
-  {#each stuffIUse as tool}
-    <img
-      src={`/${tool}.svg`}
-      alt=""
-      class="w-10 pr-1"
-      loading="lazy"
-    />
-  {/each}
-</div>
+<section class="mx-8">
+  <div
+    class="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white mt-16"
+    id="contact"
+  >
+    Stuff I Use
+  </div>
+  <div class="flex flex-wrap">
+    {#each stuffIUse as tool}
+      <img src={`/${tool}.svg`} alt="" class="w-10 pr-1" loading="lazy" />
+    {/each}
+  </div>
+</section>
