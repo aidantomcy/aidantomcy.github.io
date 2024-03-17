04d616331cf4bbd4a331efb6da9afb5d2b5b8318
Aidan Tomcy <aidantomcy@gmail.com>
2024-03-17 11:05:51 +0530

    replace splitbee analytics with vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), partytown()],
-  site: 'https://aidantomcy.github.io'
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
