9b34cb2cdc223433f30759639197dd321015b5c8
Aidan Tomcy <aidantomcy@gmail.com>
2025-04-22 12:54:45 +0530

    remove splitbee analytics and partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), partytown()],
-  site: 'https://aidantomcy.github.io'
+  integrations: [tailwind(), svelte()],
+  site: "https://aidantomcy.github.io",
 });
